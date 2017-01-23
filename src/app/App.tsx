bd9f62a2d007c711ffd9fab8d61ada34aef18405
Martin Hochel <hochelmartin@gmail.com>
2017-01-23 01:19:10 +0100

    chore(ts): add formatter and apply on files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import { h, Component } from 'skatejs';
 
 export class App extends Component<void> {
-  static get is(){ return 'my-app' }
-  renderCallback(){
+  static get is() { return 'my-app' }
+  renderCallback() {
     return [
       <div>Hello</div>
     ]
